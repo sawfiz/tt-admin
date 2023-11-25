760e39791fe79154a77947cf285630c7350e3e07
sawfiz <dashu.up@gmail.com>
2023-11-25 11:33:17 +0800

    Make reusable DynamicDetails

---
+++ src/components/DynamicDetails.jsx
@@ -0,0 +1,179 @@
+// Libraries
+import { useState, useEffect, useContext } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { format } from 'date-fns';
+
+// Contexts
+import { AuthContext } from '../contexts/AuthContext';
+import { useModal, InfoModal } from '../contexts/ModalContext';
+
+// Utilities
+import { httpRequest } from '../utils/apiServices';
+
+// Styling
+import { Button, Modal } from 'react-bootstrap';
+
+export default function DynamicDetails({type, id}) {
+  // const { id } = useParams();
+  const navigate = useNavigate();
+  const { logout } = useContext(AuthContext);
+  const { showModal, closeModal } = useModal();
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await httpRequest(
+        'GET',
+        `/api/${type}s/${id}`,
+        null,
+        type
+      );
+
+      if (response.error) {
+        // Display the model. If error is token timed out, click on button logs the user out.
+        showModal(
+          <InfoModal
+            show={true}
+            handleClose={closeModal}
+            title={response.error}
+            body={response.message}
+            primaryAction={response.status === 403 ? handleLogout : closeModal}
+          />
+        );
+        setErrorMsg(`${response.error} ${response.errorMsg}`);
+      } else {
+        setData(response);
+      }
+    };
+
+    fetchData();
+    setLoading(false);
+  }, [id]); // Include id as it is used in the useEffect
+
+  // Logout if token expired
+  const handleLogout = async () => {
+    await httpRequest('POST', '/logout');
+    closeModal();
+    logout();
+    navigate('/login');
+  };
+
+  const imgSrc = data
+    ? data.photoURL ||
+      (data.gender === 'male'
+        ? '/images/boy.png'
+        : data.gender === 'female'
+        ? '/images/girl.png'
+        : '/images/unknown.png')
+    : null;
+
+  // Function to open the delete confirmation modal
+  const handleShowDeleteModal = () => {
+    setShowDeleteModal(true);
+  };
+
+  // Function to close the delete confirmation modal
+  const handleCloseModal = () => {
+    setShowDeleteModal(false);
+  };
+
+  const handleDelete = async () => {
+    try {
+      // Send API request to delete the athlete
+      await httpRequest('DELETE', `/api/athletes/${id}`);
+      // Redirect or perform any other action upon successful deletion
+      navigate('/manage-athletes'); // Redirect to athletes page after deletion
+    } catch (error) {
+      console.error('Error deleting athlete:', error);
+      // Handle error state or show an error message to the user
+    }
+  };
+
+  const handleDeleteConfirmation = () => {
+    handleCloseModal(); // Close the modal
+    handleDelete(); // Trigger the delete function
+  };
+  return (
+    <div>
+      <Link to={`/manage-${type}s`}>
+        <Button variant="outline-secondary">⬅️ Atheltes</Button>
+      </Link>
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : errorMsg ? (
+          <p className="text-danger">{errorMsg}</p>
+        ) : (
+          data && (
+            <div>
+              {/* Athlete full name */}
+              <h2 className="mt-24 font-bold text-xl">{data.name}</h2>
+              {/* Profile photo */}
+              <div className=" absolute top-4 right-[0.5rem] w-28 h-28 overflow-hidden">
+                <img
+                  className="w-full h-full object-center object-cover rounded-lg "
+                  src={imgSrc}
+                  alt="profile"
+                />
+              </div>
+              {/* Personal details */}
+              <div className="outline-dashed outline-2 outline-pink-300 p-2 grid grid-cols-[1fr_2fr] my-2">
+                <div className=" font-bold">Active</div>
+                <div>{data.active ? '✅' : '❌'}</div>
+                <div className=" font-bold">Gender</div>
+                <div>{data.gender}</div>
+                <div className=" font-bold">Birthdate</div>
+                <div>
+                  {data.birthdate
+                    ? format(new Date(data.birthdate), 'yyyy-MM-dd')
+                    : ''}
+                </div>
+                <div className=" font-bold">Mobile</div>
+                <div>{data.mobile}</div>
+                <div className=" font-bold">Email</div>
+                <div>{data.email}</div>
+                <div className=" font-bold">School</div>
+                <div>{data.school}</div>
+                <div className=" font-bold">Father</div>
+                <div>{data.father}</div>
+                <div className=" font-bold">Mother</div>
+                <div>{data.mother}</div>
+              </div>
+
+              {/* Buttons */}
+              <div className="flex justify-around">
+                <Link to={`/athlete/update/${id}`}>
+                  <Button variant="primary">Update</Button>
+                </Link>
+                <Button variant="danger" onClick={handleShowDeleteModal}>
+                  Delete
+                </Button>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+
+      {/* Delete confirmation modal */}
+      <Modal show={showDeleteModal} onHide={handleCloseModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Deletion</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this athlete?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={handleDeleteConfirmation}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
